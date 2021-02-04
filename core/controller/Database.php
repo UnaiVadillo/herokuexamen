604210716e7dbd15e51f5ab4a1ebcdccc2e08ada
unaivadillo <unai.vadillo@ikasle.egibide.org>
2021-02-04 08:37:52 +0100

    ddbb

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,13 +3,13 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		$this->user="bdabc0a31ebf21";$this->pass="6fc0ed6a";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_47c5e4a9c8f7a03";
 		//Copiar también la información de la conexión en la función de abajo connect()
 	}
 
 	function connect(){
 		//Copiar aquí también la info de la conexión
-		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		$this->user="bdabc0a31ebf21";$this->pass="6fc0ed6a";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_47c5e4a9c8f7a03";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
